b7f95079cf6f5db2f1545cb0249641ef133c7928
KimHyeWon <keykeykey193@gmail.com>
2020-12-02 18:14:21 +0900

    ♻️ Set useEffects, axios, data, ... etc !

--- src/App.js
+++ src/App.js
@@ -1,30 +1,41 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styled, { createGlobalStyle } from "styled-components";
+import axios from "axios";
 
 
 function App() {
   const [posts, setPosts] = useState([]);
   // useState를 통해 posts이름의 state와 이를 변경하는 setPosts 함수 생성.
   // posts 에는 배열이 저장되므로 초기값으로 빈 배열 ([])을 주었다.
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      // .then((response) => console.log(response));  
+  // 컴포넌트가 렌더링(화면에 그려지는 것)될 때마다 useEffect가 실행된다.
+  // then()에서는 http 요청 통해 받아온 데이터 처리
+  // 테스트 위해 받아온 데이터 console에 출력  
+      .then(({ data }) => setPosts(data));
+  }, []);   
+  //axios를 통해 HTTP요청을 보내는 code
+  // ({ data }) 는 es6의 destructing 문법
+  // response 객체 안에 있는 data배열을 바로 참조 할 수 있다.
+  // useEffect 내부에서 state를 변경시킨 결과로 인해 컴포넌트의 재렌더링이 일어난다. 렌더링시에는 다시 useEffect가 실행되어 
+  // 렌더링 -> useEffect -> posts state 변화 -> 렌더링 -> useEffect -> posts state 변화 -> 렌더링 무한 반복
+
   return (
+  // return 내부 코드 : react element : jsx
+  // jsx 안에는 저렇게 ( { } ) 를 사용해 js 함수 사용 할 수 있다.
     <Container>
       <GlobalStyle />
-      <Post>
-      <Title>Title1</Title>
-      <Body>Body1</Body>
-      </Post>
-      <Post>
-      <Title>Title2</Title>
-      <Body>Body2</Body>
-      </Post>
-      <Post>
-      <Title>Title3</Title>
-      <Body>Body3</Body>
-      </Post>
-      <Post>
-      <Title>Title4</Title>
-      <Body>Body4</Body>
-      </Post>
+      {posts.map((post, index) => (
+  // 100개 요소를 모두 출력하기 위해 Array.ma() 메소드 사용 
+      <Post key={index}>
+  {/* key prop로 각각을 식별할 수 잇는 고유한 값 설정
+      여기선 index = key prop */}
+        <Title>{post.title}</Title>
+        <Body>{post.body}</Body>
+      </Post>      
+      ))}
     </Container>
   );
 }
